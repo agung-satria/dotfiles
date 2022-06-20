ca5ba4be2defc83949f04f57b6815cf23a0c79cf
Agung Satria <agungsatria890@gmail.com>
2022-06-20 11:33:31 +0700

    picom with transition-blur, font-arimoNF, dwmOK

--- suckless/dwmhidevacant/config.h
+++ suckless/dwmhidevacant/config.h
@@ -9,14 +9,14 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 1;        /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 9;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 9;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 13;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 13;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 13;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 13;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
 /*  monocle mode in the presence of several windows.                        */
@@ -25,10 +25,10 @@ enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always
 static const int showtab			= showtab_auto;        /* Default tab bar show mode */
 static const int toptab				= False;               /* False means bottom tab bar */
 
-static const char *fonts[]        = { "Iosevka Nerd Font:style:medium:size=14",
+static const char *fonts[]        = { "Arimo Nerd Font:style:medium:size=14",
                                       "JoyPixels:size=14:antialias=true:autohint=true", 
                                       "Material Design Icons-Regular:size=14"};
-static const char dmenufont[]     = "Iosevka Nerd Font:style:medium:size=14";
+static const char dmenufont[]     = "JetBrainsMono Nerd Font:style:medium:size=14";
 static const char col_gray1[]     = "#141919";
 static const char col_gray2[]     = "#39393c";
 static const char col_gray3[]     = "#bbbbbb";
