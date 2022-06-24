0a3afb7912efe21ddc4e72dff210aca3520aef1a
Agung Satria <agungsatria890@gmail.com>
2022-06-24 18:31:09 +0700

    tmux, dwm

--- suckless/dwmhidevacant/config.h
+++ suckless/dwmhidevacant/config.h
@@ -13,7 +13,7 @@ static const unsigned int gappih    = 10;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
@@ -239,7 +239,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = rofi } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY|ControlMask,           XK_Return, spawn,    SHCMD("tabbed -r 2 st -w ''") },
+  //{ MODKEY|ControlMask,           XK_Return, spawn,    SHCMD("tabbed -r 2 st -w ''") },
+  { MODKEY|ControlMask,           XK_Return, spawn,    SHCMD("tabbed -r 2 st -w '' -e tmux") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ControlMask,           XK_w,      tabmode,        {-1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
--- suckless/dwmhidevacant/dwm.c
+++ suckless/dwmhidevacant/dwm.c
@@ -2266,14 +2266,14 @@ resizeclient(Client *c, int x, int y, int w, int h)
 
 	wc.border_width = c->bw;
 /* ____________noborder____________*/
-	// if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
-	//     || &monocle == c->mon->lt[c->mon->sellt]->arrange)
-	//     && !c->isfullscreen && !c->isfloating
-	//     && NULL != c->mon->lt[c->mon->sellt]->arrange) {
-	// 	c->w = wc.width += c->bw * 2;
-	// 	c->h = wc.height += c->bw * 2;
-	// 	wc.border_width = 0;
-	// }
+	if (((nexttiled(c->mon->clients) == c && !nexttiled(c->next))
+	    || &monocle == c->mon->lt[c->mon->sellt]->arrange)
+	    && !c->isfullscreen && !c->isfloating
+	    && NULL != c->mon->lt[c->mon->sellt]->arrange) {
+		c->w = wc.width += c->bw * 2;
+		c->h = wc.height += c->bw * 2;
+		wc.border_width = 0;
+	}
 /* ____________noborder____________*/
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
